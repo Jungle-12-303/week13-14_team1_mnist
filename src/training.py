6ccbb867ec2bcdbfb78d8a0e58d3036ecbb46cf6
iamnuked <qjawls6262@gmail.com>
2026-05-26 11:06:55 +0900

    feat: training 완성

--- src/training.py
+++ src/training.py
@@ -20,6 +20,39 @@ def train(model, optimizer, x_train, y_train, epochs=20, batch_size=128):
     """
     # TODO: epoch마다 데이터를 섞고, batch 단위로 forward/loss/backward/update를 수행하세요.
     # 힌트: Softmax + CrossEntropy 결합 gradient는 y_pred copy에서 정답 위치에 1을 빼서 만듭니다.
+    num_samples = x_train.shape[0]
+    loss_history = []
+    
+    for _ in range(epochs):
+        indices = np.random.permutation(num_samples)
+
+        x_shuffled = x_train[indices]
+        y_shuffled = y_train[indices]
+
+        epoch_loss = 0
+
+        for start in range(0, num_samples, batch_size):
+            end = start + batch_size
+            x_batch = x_shuffled[start:end]
+            y_batch = y_shuffled[start:end]
+
+            y_pred = model.forward(x_batch, train=True)
+            loss = cross_entropy_loss(y_pred, y_batch)
+
+            dout = y_pred.copy()
+            dout[np.arange(x_batch.shape[0]), y_batch] -= 1
+            dout /= x_batch.shape[0]
+
+            model.backward(dout)
+            optimizer.update(model.params, model.grads)
+
+            epoch_loss += loss * x_batch.shape[0]
+
+
+        loss_history.append(epoch_loss / num_samples)
+
+    return loss_history
+
     raise NotImplementedError("train을 구현하세요.")
 
 
